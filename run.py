66cd13342e749adbffc09bd26222a5395754630b
mrpycharm <ahmed.dhanani@tutanota.com>
2017-10-19 12:04:15 +0500

    proper commenting for readability

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
+""" This file needs to be run to start the server.
+	For now we are just initalizing the app.
+	We are going to use a virtualenv later.
+""" 
+
+
 def run_app():
+	# import app
 	from sarahah import app
 
+	# run app
 	app.run(debug=True)
-
 
 
 if __name__ == '__main__':
--- sarahah/views.py
+++ sarahah/views.py
@@ -1,6 +1,10 @@
+# ---  package imports
 from sarahah import app
 
 
+# --- main views
+
 @app.route('/')
 def hello_world():
+	""" basic hello world view """
 	return 'Hello World'
